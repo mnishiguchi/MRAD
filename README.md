05ca83fe8223c45ceb2dd3876eff93172d77f887
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2016-01-14 09:30:07 -0500

    Basic functionalities

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # MRAD
-- I want to learn how to design websites through analyzing Mr. Mrad's beautiful portfolio at http://rachidmrad.com/.
+- I will learn how to design websites through analyzing Mr. Mrad's beautiful portfolio at http://rachidmrad.com/.
 
 ## Things I want to learn
-- Where does portfolio.json is stored?
+
 - What types of images are required for the purpose of animation?
-- The mechanics of responsive sliding navidation
-- How to implement images that respond to mouse hover.
+- The mechanics of responsive sliding navidation (custom-navbar header)
+- Animated icons (e.g. +, =)
+- How to implement images that respond to mouse hover?
+- How the Hexagon shapes are re-organized when the screen size changes?
 - [ngAnimate](https://docs.angularjs.org/api/ngAnimate)
 
 ## Copyright
--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 
 /**
- * Responds to mouseclicks on the nav-buttons.
+ * jQuery code
+ * Allows nav-buttons to respond to mouseclicks.
  */
 $( document  ).ready( function () {
 
@@ -33,6 +34,9 @@ $( document  ).ready( function () {
 // ---------------------------------------------- //
 
 
+/**
+ * Angular code.
+ */
 (function() {
 
   // Declare the app module.
@@ -47,6 +51,25 @@ $( document  ).ready( function () {
   // -------------------------------------------- //
 
 
+  /**
+   * Manages app-wide scope.
+   */
+  angular
+    .module( 'app' )
+    .controller( 'AppController', AppController );
+
+    AppController.$inject = [];
+
+    function AppController() {}
+
+
+  // -------------------------------------------- //
+  // -------------------------------------------- //
+
+
+  /**
+   * Defines the routes.
+   */
   angular
     .module('app' )
     .config( config );
@@ -54,16 +77,17 @@ $( document  ).ready( function () {
     config.$inject = [
       "$routeProvider"
     ];
+
     function config( $routeProvider ) {
 
       $routeProvider
         .when('/portfolio', {
           templateUrl: 'partials/_portfolio.html',
-          controller: 'PortfolioController'
+          controller: 'PortfolioController as portfolio'
         })
         .when('/project/:workId', {
           templateUrl: 'partials/_project.html',
-          controller: 'ProjectController'
+          controller: 'ProjectController as project'
         })
         .when('/about', {
           templateUrl: 'partials/_about.html',
@@ -84,33 +108,9 @@ $( document  ).ready( function () {
   // -------------------------------------------- //
 
 
-  angular
-    .module( 'app' )
-    .value( 'version', '0.1' );
-
-
-  // -------------------------------------------- //
-  // -------------------------------------------- //
-
-
-  angular
-    .module( 'app' )
-    .filter( 'interpolate', interpolate );
-
-    interpolate.$inject = [
-      'version'
-    ];
-    function interpolate( version ) {
-      return function( text ) {
-        return String( text  ).replace( /\%VERSION\%/mg, version );
-      };
-    }
-
-
-  // -------------------------------------------- //
-  // -------------------------------------------- //
-
-
+  /**
+   * Controls the portpolio.
+   */
   angular
     .module( 'app' )
     .controller( 'PortfolioController', PortfolioController );
@@ -118,6 +118,7 @@ $( document  ).ready( function () {
     PortfolioController.$inject = [
       '$http'
     ];
+
     function PortfolioController( $http ) {
 
       var vm = this;
@@ -125,7 +126,8 @@ $( document  ).ready( function () {
       // Initial state.
       vm.portfolio = [];
 
-      getData();
+      // Load the portfolio data into vm.
+      vm.portfolio = getPortfolioData();
 
 
       // ---
@@ -136,39 +138,56 @@ $( document  ).ready( function () {
       /**
        * Fetch the data for the entire work and store it into vm.portfolio.
        */
-      function getData() {
+      function getPortfolioData() {
 
-        $http
-          .get( 'data/portfolio.json' )
-          .success( function( data ) {
-            vm.portfolio = data;
-          });
+        // $http
+        //   .get( 'data/portfolio.json' )
+        //   .success( function( data ) {
+        //     vm.portfolio = data;
+        //   });
 
-      }
+        // Return a stub list.
+        return [
+          {
+            name: "item_1",
+            desc: "desc_1"
+          },
+          {
+            name: "item_2",
+            desc: "desc_2"
+          },
+          {
+            name: "item_3",
+            desc: "desc_3"
+          }
+        ];
 
-    }
+      } // end getPortfolioData
+
+    } // end PortfolioController
 
 
+  /**
+   * Controls the projects.
+   */
   angular
     .module( 'app' )
     .controller( 'ProjectController', ProjectController );
 
     ProjectController.$inject = [
       '$routeParams',
-      '$http',
-      '$location',
-      '$anchorScroll'
+      '$http'
     ];
-    function ProjectController( $routeParams, $http, $location, $anchorScroll ) {
+
+    function ProjectController( $routeParams, $http ) {
 
       var vm = this;
 
       // Initial state.
       vm.work = [];
 
-      $anchorScroll();
-
-      getItem();
+      // Get a list of work based on the workID passed in as a param.
+      vm.work = getWork();
 
 
       // ---
@@ -177,18 +196,91 @@ $( document  ).ready( function () {
 
 
       /**
-       * Fetch the data for the work that is specified by the workId.
+       * Fetches the data for the work that is specified by the workId in the
+       * routeParams.
        */
-      function getItem() {
+      function getWork() {
 
-        $http
-          .get( 'data/' + $routeParams.workId + '.json' )
-          .success( function( data ) {
-            vm.work = data;
-          });
-        }
+        // $http
+        //   .get( 'data/' + $routeParams.workId + '.json' )
+        //   .success( function( data ) {
+        //     vm.work = data;
+        //   });
+        // }
 
+        // A stub list of the item lists.
+        var workLists = [
+          // [0]
+          [
+            {
+              name: "Ruby",
+              desc: "Ruby programming"
+            },
+            {
+              name: "JavaScript",
+              desc: "JS programming"
+            },
+          ],
+          // [1]
+          [
+            {
+              name: "Moving estimator",
+              desc: "An android app"
+            },
+            {
+              name: "Sample app",
+              desc: "A web app"
+            },
+          ],
+
+        ]; // end workLists
+
+        return workLists[ $routeParams.workId ];
+
+      } // end getWork
+
+    } // end ProjectController
+
+
+  // -------------------------------------------- //
+  // -------------------------------------------- //
+
+
+  /**
+   * Defines the app version.
+   */
+  angular
+    .module( 'app' )
+    .value( 'version', '0.1' );
+
+
+  // -------------------------------------------- //
+  // -------------------------------------------- //
+
+
+  /**
+   * Interpolates the app version number into the passed-in string.
+   * Insertion points must be specified by the word "%VERSION%".
+   */
+  angular
+    .module( 'app' )
+    .filter( 'interpolateAppVersion', interpolateAppVersion );
+
+    interpolateAppVersion.$inject = [
+      'version'
+    ];
+
+    function interpolateAppVersion( version ) {
+
+      var filter = function( text ) {
+
+        // g: global match
+        // m: match over multiple lines
+        return String( text  ).replace( /\%VERSION\%/mg, version );
       }
+
+      return filter;
+    }
 
 
   // -------------------------------------------- //
@@ -206,6 +298,7 @@ $( document  ).ready( function () {
     appVersion.$inject = [
       'version'
     ];
+
     function appVersion( version ) {
       return link;
     }
@@ -214,33 +307,4 @@ $( document  ).ready( function () {
       elm.text( version );
     }
 
-
-  // -------------------------------------------- //
-  // -------------------------------------------- //
-
-
-  /**
-   * resetViewOnclick
-   * Resets the view when clicked.
-   */
-  angular
-    .module( 'app' )
-    .directive( 'resetViewOnclick', resetViewOnclick );
-
-    resetViewOnclick.$inject = [
-      "$location"
-    ];
-    function resetViewOnclick( $location ) {
-      return link;
-    }
-
-    /**
-     * Binds the element to mouse click.
-     */
-    function link ( scope, element, attrs ) {
-      element.click( function() {
-        $location.path( 'portfolio/#' );  // # to go to top
-      });
-    };
-
-})();
+})(); // end module
